a809d0511e1b70ac1a87679a70f8ab8b3e8339c5
kazuki <kazukishiroma06@gmail.com>
2026-03-02 11:10:56 -0300

    Adiciona emprestar e devolver no ItemBiblioteca

--- item_biblioteca.py
+++ item_biblioteca.py
@@ -11,3 +11,43 @@ class Item_biblioteca(ABC):
     def exibir_detalhes(self):
         pass
 
+    def emprestar(self, disponivel: bool):
+        if disponivel == True:
+            self.__disponivel = False
+            print(f"O livro foi emprestado com sucesso")
+        print(f"O livro já está sendo emprestando 😢")
+
+    def devolver(self, disponivel: bool):
+        if disponivel == False:
+            self.__disponivel = True
+            print(f"O livro foi devolvido com sucesso!")
+        print(f"O livro já foi devolvido!")
+
+    def set_codigo(self, codigo: int):
+        if codigo > 0:
+            self.__codigo = codigo
+        print(f"Código invalido! {codigo} deve ser maior que 0")
+
+    def set_titulo(self, titulo: str):
+        if not titulo.strip():
+            print(f"O título está vazio ou contém apenas espaços")
+        self.__titulo = titulo
+        print(f"Titulo {titulo} cadastrado!")
+    
+    def set_ano(self, ano: int):
+        if ano > 0:
+            self.__ano = ano
+            print(f"Ano cadastrado!")
+        print(f"Ano invalido!")
+
+    def get_codigo(self):
+        return self.__codigo
+    
+    def get_titulo(self):
+        return self.__titulo
+    
+    def get_ano(self):
+        return self.__ano
+    
+    def get_disponivel(self):
+        return self.__disponivel
