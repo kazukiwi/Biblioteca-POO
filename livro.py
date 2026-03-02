b3a25fcf664e418153e40454c93e9687f316a693
kazuki <kazukishiroma06@gmail.com>
2026-03-02 13:05:33 -0300

    Criação classe Revista

--- livro.py
+++ livro.py
@@ -3,7 +3,7 @@ from item_biblioteca import Item_biblioteca
 class Livro(Item_biblioteca):
     def __init__(self, codigo, titulo, ano, disponivel, autor: str, num_paginas: int):
         super().__init__(codigo, titulo, ano, disponivel)
-        self.item = "livro"
+        self.item = "Livro"
         self.__autor = autor
         self.__num_paginas = num_paginas
 
@@ -13,5 +13,3 @@ class Livro(Item_biblioteca):
         print(f"Autor: {self.__autor}\n"
               f"Numero de Páginas: {self.__num_paginas}")
         
-livro = Livro(9, "Percy Jackson", 2005, True, "Rick Riordan", 400)
-livro.exibir_detalhes()
---
+++ revista.py
@@ -0,0 +1,15 @@
+from item_biblioteca import Item_biblioteca
+
+class Revista(Item_biblioteca):
+    def __init__(self, codigo, titulo, ano, disponivel, edicao: int, mes: str):
+        super().__init__(codigo, titulo, ano, disponivel)
+        self.item = "Revista"
+        self.__edicao = edicao
+        self.__mes = mes
+
+    def exibir_detalhes(self):
+        print(f"Item Biblioteca: {self.item}")
+        super().exibir_detalhes()
+        print(f"Autor: {self.__edicao}\n"
+              f"Numero de Páginas: {self.__mes}")
+        
